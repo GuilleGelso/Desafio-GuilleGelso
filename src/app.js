a7f409dbfe613f0025f8460ab6a28c29fe4a3edb
Guille Gelso <celidiog@gmail.com>
2023-11-01 15:23:22 -0300

    desafio

--- src/app.js
+++ src/app.js
@@ -1,18 +1,46 @@
-import express from "express"
-import productsRouter from "./routes/products.router.js"
-import cartsRouter from "./routes/carts.router.js"
+import express from 'express'
+import handlebars from 'express-handlebars'
+import { Server } from 'socket.io'
+import __dirname from './utils.js'
+import { PORT } from './utils.js'
+import productsRouter from './routes/products.router.js'
+import cartsRouter from './routes/carts.router.js'
+import viewsRouter from './routes/views.router.js'
+
 
 const app = express()
 app.use(express.json())
-
-app.get("/", (req, res) => res.status(200).json({ message: "Server Ok" }))
-
+// app.use(express.urlencoded({ extended: true }))
 
 
-app.use("/api/products", productsRouter)
-app.use("/api/carts", cartsRouter)
+const httpServer = app.listen(PORT, () => console.log(`Server is running at http://localhost:${PORT}`))
+httpServer.on('error', err => console.log(`Express server error: ${err.message}`))
+
+const io = new Server(httpServer)
+
+app.set('socketio', io)
+/* app.use((req, res, next) => {
+    req.io = io
+    next()
+}) */
+
+app.use(express.static(`${__dirname}/public`))
+
+app.engine('handlebars', handlebars.engine())
+app.set('view engine', 'handlebars')
+app.set('views', `${__dirname}/views`)
 
 
-const PORT = process.env.PORT || 8080
-const server = app.listen(PORT, () => console.log(`Server is running at http://localhost:${PORT}`))
-server.on('err', err => console.log(`Express server error: ${err.message}`))
+app.get('/', (req, res) => res.status(200).render('index', { name: 'profe'}))
+app.use('/api/products', productsRouter)
+app.use('/api/carts', cartsRouter)
+// app.use('/products', viewsRouter)
+app.use('/home', viewsRouter)
+
+
+io.on('connection', socket => {
+    console.log('New client connected to the Server')
+    socket.on('productList', data => {
+        io.emit('updatedProducts', data)
+    })
+})
